d574755f53b96de5727162da0fa654423e70365b
Severin Smith <info@smallbutdigital.com>
2012-09-30 17:37:27 -0400

    Made nice plots for the report.
    This includes experimental and thoretical data

--- analysis/lab1/FIRDesign.py
+++ analysis/lab1/FIRDesign.py
@@ -1,6 +1,3 @@
-#Important this assignment was done in PYLAB
-#NOT MATLAB
-
 from pylab import *
 from scipy.signal import *
 
@@ -16,38 +13,42 @@ def printArray(val):
 	string = string+"}"
 	print string
 
-def Q3():
+def kaiserplot():
 	kaiser_param = kaiserord(db(0.05), 0.05)
-	
+
 	taps = firwin(kaiser_param[0]+1, [0.0625, 0.8375], window=('kaiser', kaiser_param[1]), pass_zero=False)
 	# printArray(taps)
 
 	resp = fft(taps, 1000)
 
-	subplot(2, 1, 1)
-
 	title("Kaiser Window, FIR Bandpass filter")
-	xlabel("z")
-	ylabel("|Hda(z)|")
-	plot([8000.0/size(resp)*i for i in range(size(resp))], abs(array(resp)))
+	xlabel("Frequency (Hz)")
+	ylabel("Gain (dB)")
+	ylim([-60, 5])
+	plot([8000.0/size(resp)*i for i in range(size(resp))], db(abs(array(resp))), linewidth=2.5)
 	grid()
 
-def Q4():
-	M = 64
+def remezplot(M, bhp):
 
-	taps = remez(int(M), [0, 0.050000000000000003, 0.074999999999999997, 0.82499999999999996, 0.84999999999999998, 1], [0, 1, 0], [1, 1, 1], 2, 'hilbert')
+	taps = remez(int(M), [0, 0.050000000000000003, 0.074999999999999997, 0.82499999999999996, 0.84999999999999998, 1], [0, 1, bhp], [1, 1, 1], 2, 'hilbert')
 	# printArray(taps)	
 
 	resp = fft(taps, 1000)
 
-	subplot(2, 1, 2)
-
-	title("Parks-McClellan/Remez Exchange, FIR Bandpass filter, Initial Order Estimate")
-	xlabel("z")
-	ylabel("|Hda(z)|")
-	plot([8000.0/size(resp)*i for i in range(size(resp))], abs(array(resp)))
+	title("Parks-McClellan, FIR Bandpass filter")
+	xlabel("Frequency (Hz)")
+	ylabel("Gain (dB)")
+	ylim([-60, 5])
+	plot([8000.0/size(resp)*i for i in range(size(resp))], db(abs(array(resp))), linewidth=2.5)
 	grid()
 
-Q3()
-Q4()
+subplot(2, 2, 1)
+kaiserplot()
+subplot(2, 2, 2)
+remezplot(128, 0)
+subplot(2, 2, 3)
+remezplot(64, 0)
+subplot(2, 2, 4)
+remezplot(64, 1)
+
 show()
--- analysis/lab1/ResultAnaylsis.py
+++ analysis/lab1/ResultAnaylsis.py
@@ -1,5 +1,10 @@
 from pylab import *
+from scipy.signal import *
 import csv
+
+#Compute dBs
+def db(val):
+	return 20*log10(val)
 
 freq = []
 input_p_p = []
@@ -7,23 +12,42 @@ input_rms = []
 output_p_p = []
 output_rms = []
 
+def kaiserplot():
+	kaiser_param = kaiserord(db(0.01), 0.05)
+
+	taps = firwin(kaiser_param[0]+1, [0.0625, 0.8375], window=('kaiser', kaiser_param[1]), pass_zero=False)
+	# printArray(taps)
+
+	resp = fft(taps, 1000)
+
+	plot([8000.0/size(resp)*i for i in range(size(resp)/2)], db(abs(array(resp[0:size(resp)/2]))), label="Ideal", linewidth=2.5)
+
 with open('FIRTransferFunction.csv', 'rb') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        if row[0] != "#":
-            freq.append(float(row[0]))
-            input_p_p.append(float(row[1]))
-            input_rms.append(float(row[2]))
-            output_p_p.append(float(row[3]))
-            output_rms.append(float(row[4]))
+	reader = csv.reader(file)
+	for row in reader:
+		if row[0][0] != "#":
+			freq.append(float(row[0]))
+			input_p_p.append(float(row[1]))
+			input_rms.append(float(row[2]))
+			output_p_p.append(float(row[3]))
+			output_rms.append(float(row[4]))
 
+freq = array(freq)
+input_p_p = array(input_p_p)
+input_rms = array(input_rms)
+output_p_p = array(output_p_p)
+output_rms = array(output_rms)
 
+plot(freq, db(output_rms/input_rms), label="Experiment", linewidth=2.5)
+# fill_between(freq, 0, output_p_p, facecolor='green')
 
-plot(freq, input_p_p)
-#plot(freq, input_rms)
-plot(freq, output_p_p)
-fill_between(freq, 0, output_p_p, facecolor='green')
-#plot(freq, output_rms)
-#xlim([10, 200])
+kaiserplot()
+
+title("Kaiser Window, FIR Band-Pass Filter")
+legend()
+xlabel("Frequency (Hz)")
+ylabel("Gain (dB)")
+ylim([-30, 5])
+grid()
 
 show()
