3d076e2690dea5a5d42a4238198b45c6ae6ca732
Severin Smith <info@smallbutdigital.com>
2012-11-19 20:22:37 -0500

    Added C shell for the project

---
+++ computer/project/levinsondurbin.h
@@ -0,0 +1,9 @@
+// ECSE 436 - Signal Processing Hardware
+// Lab 2
+// Salenikovich, Stepan - 260326129
+// Smith, Severin - 260349085
+
+#ifndef LEVINSONDURBIN_H
+#define LEVINSONDURBIN_H
+
+#endif
---
+++ computer/project/lpc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lpc.h"
+#include "classify.h"
+#include "levinsondurbin.h"
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello world\n");
+
+	return 0;
+}
