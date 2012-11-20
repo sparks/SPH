57036118319856ef7d491eef11bdb6eb96810bc4
Stepan Salenikovich <stepan.salenikovich@gmail.com>
2012-11-20 04:35:10 -0500

    added file read and write:

--- computer/project/lpc.c
+++ computer/project/lpc.c
@@ -14,33 +14,66 @@ float e[BLOCKSIZE];		// error values
 
 //I/O buffers
 short DATA_IN[BLOCKSIZE];
-short DATA_OUT[BLOCKSIZE];
-short DATA_OUT_OLD[BLOCKSIZE];
+short DATA_OUT_1[BLOCKSIZE];
+short DATA_OUT_2[BLOCKSIZE];
+short *data_out;
+short *data_out_old;
 
-int main(int argc, char const *argv[])
-{
-	printf("Hello world\n");
+int main() {
+	FILE *infile, *outfile;
+	int datacount;
+	short *swap;
 
-	//get audio
+    /* Open the input file and quit if fail */
+	infile = fopen(IN_FILENAME, "rb");
+	if (!infile) {
+		printf("fopen for reading failed with %d!\n", errno);
+		return 0;
+	}
+
+    /* Open the output file and quit if fail */
+	outfile = fopen(OUTPUT_FILENAME,"wb");
+	if (!outfile) {
+		printf("fopen for writing failed with %d!\n", errno);
+		return 0;
+	}
+
+	reset();
 	
-	//for loop processing audio
-	//{
-	//	process block of audio
-	//	{
-	//		get coef and error and stuff
-	//		classification stuff?
-	//		
-	//	}
-	//
-	//	"receive" encoded block
-	//	synthesize block of audio
-	//	{
-	//		
-	//	}
-	//	
-	//}
+	//init out pointers
+	data_out = DATA_OUT_1;
+	data_out_old = DATA_OUT_2;
 
-	return 0;
+	//Read in NN chunk
+	do {
+		datacount = fread(DATA_IN, sizeof(short), BLOCKSIZE, infile);
+		
+		// encode the block for the receiver
+		encode_block(DATA_IN, a, NUMCOEF, e, BLOCKSIZE);
+
+		synthesize_block(a, NUMCOEF, e, BLOCKSIZE, data_out, data_out_old);
+
+		// wrtie out to file
+		fwrite(data_out, sizeof(short), datacount, outfile);
+		
+		//switch data out and data out old
+		*swap = out;
+		out = out_old;
+		out_old = swap;
+	} while (datacount == BLOCKSIZE);
+
+    /* Close the input and output files, this also flushes all
+    * pending I/O, so that other programs can access the data. */
+	fclose(infile);
+	fclose(outfile);
+}
+
+void reset(void) {
+	int i;
+
+	for(i = 0;i < BLOCKSIZE;i++) {
+		DATA_OUT_OLD[i] = 0;
+	}
 }
 
 void encode_block(short in[], float a[], int numcoef, float e[], int blocksize){
